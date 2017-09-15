4f48e93b82ae5b28790a15221bfbc68fe34b4e9c
Anna Popovychenko <a.popovychenko@gmail.com>
2017-09-15 11:29:35 +0300

    fix

--- CrowdboticsTest/wsgi.py
+++ CrowdboticsTest/wsgi.py
@@ -10,10 +10,10 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CrowdboticsTest.settings")
 
 # application = get_wsgi_application()
 # for Heroku
-application = WhiteNoise(get_wsgi_application())
+application = DjangoWhiteNoise(get_wsgi_application())
