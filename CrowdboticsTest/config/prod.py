71affd403b9b1af0f2a344fff83d15120aaa268f
Anna Popovychenko <a.popovychenko@gmail.com>
2017-09-15 11:12:53 +0300

    reorganize settings

---
+++ CrowdboticsTest/config/prod.py
@@ -0,0 +1,25 @@
+from .base import *
+
+DEBUG = False
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+ALLOWED_HOSTS = ['.herokuapp.com']
+
+# for Heroku
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+    }
+}
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=None)
+DATABASES['default'].update(db_from_env)
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
